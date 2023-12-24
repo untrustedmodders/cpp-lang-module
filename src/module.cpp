d000a5e3f81ae07ad3e287bd7609d55c0f1b225b
Maxim Telezhenko <zlo911111@gmail.com>
2023-12-25 01:25:05 +0300

    Create module stub. CMake scripts

---
+++ src/module.cpp
@@ -0,0 +1,53 @@
+#include <wizard/plugin.h>
+#include <wizard/language_module.h>
+
+using namespace wizard;
+
+namespace cpplm {
+	class CppLanguageModule final : public ILanguageModule {
+	public:
+		CppLanguageModule() = default;
+
+		void* GetNativeMethod(std::string_view method_name) {
+			// TODO: find in _nativesMap
+			return nullptr;
+		}
+
+		// ILanguageModule
+		bool Initialize() override {
+			return true;
+		}
+
+		void Shutdown() override {
+		}
+
+		void* GetMethod(const std::string& name) override {
+			return nullptr;
+		}
+
+		void OnNativeAdded(/* data */) override {
+			// TODO: Add to natives map
+		}
+
+		LoadResult OnLoadPlugin(const IPlugin& plugin) override {
+			return {};
+		}
+
+		void OnStartPlugin(const IPlugin& plugin) override {
+			// TODO: implement
+		}
+
+	private:
+		//TODO: _nativesMap
+	};
+
+	CppLanguageModule g_cpplm;
+
+	__declspec(dllexport) void* GetNativeMethod(std::string_view method_name) {
+		return g_cpplm.GetNativeMethod(method_name);
+	}
+}
+
+__declspec(dllexport) wizard::ILanguageModule* GetLanguageModule() {
+	return &cpplm::g_cpplm;
+}
