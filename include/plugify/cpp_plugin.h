f8421de7549be2b4aa518091c26823bc45ab051c
Nikita Ushakov <nikita.ushakov0000@outlook.com>
2024-06-11 23:37:22 +0100

    Add missing structs

--- include/plugify/cpp_plugin.h
+++ include/plugify/cpp_plugin.h
@@ -47,3 +47,27 @@ namespace plugify {
 		return interface_addr; \
 	} \
 }
+
+namespace plugify {
+	struct Vector2 {
+		float x{};
+		float y{};
+	};
+
+	struct Vector3 {
+		float x{};
+		float y{};
+		float z{};
+	};
+
+	struct Vector4 {
+		float x{};
+		float y{};
+		float z{};
+		float w{};
+	};
+
+	struct Matrix4x4 {
+		float m[4][4]{};
+	};
+}
